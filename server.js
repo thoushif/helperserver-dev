d66610a0eef319dab5286613da36d0c7fee8df05
Thoushif Aazam Shaik <aazam555@gmail.com>
2021-06-03 22:29:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
