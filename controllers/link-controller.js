75bc66c25eee04164c742c8bf5c5d81422e82da4
Thoushif Aazam Shaik <aazam555@gmail.com>
2021-06-05 17:43:42 -0500

    initial commit

--- controllers/link-controller.js
+++ controllers/link-controller.js
@@ -1,13 +1,32 @@
 // const { createHelp } = require("../services/help.service");
-const { _getLinks, _addNewGiveLink } = require("../services/link.service");
+const {
+  _getGiveLinks,
+  _addNewGiveLink,
+  _getGiveLinkById,
+  _getAskLinks,
+  _getAskLinkById,
+  _addNewAskLink,
+  _checkLinkExists
+} = require("../services/link.service");
 
-/*
- * call other imported services, or same service but different functions here if you need to
- */
-const getAllLinks = async (req, res, next) => {
+const getAllGiveLinks = async (req, res, next) => {
   let links;
   try {
-    await _getLinks().then((data) => (links = data));
+    await _getGiveLinks().then((data) => (links = data));
+
+    res.status(200).json({
+      status: "success",
+      data: { links }
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const getGiveLinkById = async (req, res, next) => {
+  let links;
+  try {
+    await _getGiveLinkById(req.params.id).then((data) => (links = data));
 
     res.status(200).json({
       status: "success",
@@ -31,7 +50,72 @@ const addNewGiveLink = async (req, res, next) => {
   }
 };
 
+const getAllAskLinks = async (req, res, next) => {
+  let links;
+  try {
+    await _getAskLinks().then((data) => (links = data));
+
+    res.status(200).json({
+      status: "success",
+      data: { links }
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const getAskLinkById = async (req, res, next) => {
+  let links;
+  try {
+    await _getAskLinkById(req.params.id).then((data) => (links = data));
+
+    res.status(200).json({
+      status: "success",
+      data: { links }
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const checkLinkExists = async (req, res, next) => {
+  let linktype = req.params.linktype;
+  let itemId = req.params.itemid;
+  let userId = req.params.userid;
+  let links;
+  try {
+    await _checkLinkExists(linktype, itemId, userId).then(
+      (data) => (links = data)
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: { links }
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const addNewAskLink = async (req, res, next) => {
+  let helps;
+  try {
+    await _addNewAskLink(req.body).then((data) => (helps = data));
+    res.status(201).json({
+      status: "success",
+      data: { helps }
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
-  getAllLinks,
-  addNewGiveLink
+  getAllGiveLinks,
+  getGiveLinkById,
+  addNewGiveLink,
+  getAllAskLinks,
+  getAskLinkById,
+  addNewAskLink,
+  checkLinkExists
 };
--- routes/link.js
+++ routes/link.js
@@ -4,7 +4,12 @@ const link = require("../controllers/link-controller");
 
 const router = express.Router();
 
-router.get("/all", link.getAllLinks);
-router.post("/add", link.addNewGiveLink);
+router.get("/give/all", link.getAllGiveLinks);
+router.get("/give/all/:id", link.getGiveLinkById);
+router.post("/give/add", link.addNewGiveLink);
+router.get("/ask/all", link.getAllAskLinks);
+router.get("/ask/all/:id", link.getAskLinkById);
+router.post("/ask/add", link.addNewAskLink);
 
+router.get("/checkLinkExists/:linktype/:itemid/:userid", link.checkLinkExists);
 module.exports = router;
